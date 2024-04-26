a5481d0003ccd5aa47c1e1e92c32e974ad1748e0
Kateryna-Ivanova <katerinadialog@gmail.com>
2024-04-26 16:20:30 +0300

    Finish
    This code ensures that the feedback statistics persist between page reloads by storing the data in local storage

--- src/App.jsx
+++ src/App.jsx
@@ -62,28 +62,105 @@
 
 // import clsx from "clsx";
 
+// ................................. ПРАЦЮЮЧИЙ ВАРІАНТ БЕЗ ЗБЕРЕЖЕННЯ ДАННИХ МІЖ СТОРІНКАМИ
+
+// import "./App.css";
+// import { useState } from "react";
+// import { useEffect } from "react";
+// import { Descriptions } from "./components/Descriptions/Descriptions/Descriptions";
+// import { Options } from "./components/Descriptions/Options/Options";
+// import { FeedBack } from "./components/Descriptions/FeedBack/FeedBack";
+// import { Notification } from "./components/Notification/Notification";
+
+// export function App() {
+//   const [clicks, setClicks] = useState({
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   });
+
+//   useEffect(() => {
+//     window.localStorage.setItem("feedback", JSON.stringify(clicks));
+//   }, [clicks]);
+
+//   const updateFeedback = (feedbackType) => {
+//     console.log(feedbackType);
+//     setClicks({
+//       ...clicks,
+//       [feedbackType]: clicks[feedbackType] + 1,
+//     });
+//   };
+
+//   const feedbackReset = () => {
+//     setClicks({
+//       ...clicks,
+//       good: (clicks.good = 0),
+//       neutral: (clicks.neutral = 0),
+//       bad: (clicks.bad = 0),
+//     });
+//   };
+
+//   const totalFeedback = clicks.good + clicks.neutral + clicks.bad;
+//   console.log(clicks);
+//   return (
+//     <>
+//       <Descriptions />
+//       <Options
+//         onClickGood={() => updateFeedback("good")}
+//         onClickNeutral={() => updateFeedback("neutral")}
+//         onClickBed={() => updateFeedback("bad")}
+//         onClickReset={feedbackReset}
+//         valueTotal={totalFeedback}
+//       />
+//       {totalFeedback >= 1 ? (
+//         <FeedBack
+//           valueTotal={totalFeedback}
+//           valueGood={clicks.good}
+//           valueNeutral={clicks.neutral}
+//           valueBad={clicks.bad}
+//         />
+//       ) : (
+//         <Notification />
+//       )}
+//     </>
+//   );
+// }
+
+// export default App;
+
 import "./App.css";
 import { useState } from "react";
+import { useEffect } from "react";
 import { Descriptions } from "./components/Descriptions/Descriptions/Descriptions";
 import { Options } from "./components/Descriptions/Options/Options";
 import { FeedBack } from "./components/Descriptions/FeedBack/FeedBack";
 import { Notification } from "./components/Notification/Notification";
 
 export function App() {
-  const [clicks, setClicks] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
+  // State Initialization with Local Storage:
+  // Стан useState.
+  // Початковий стан встановлюється на значення, отримане з локального сховища за ключем "feedback".
+  // Якщо в локальному сховищі не знайдено жодних даних, початковий стан встановлюється на об’єкт із властивостями good, neutral і bad, усі ініціалізуються на 0.
+  // Це гарантує збереження даних зворотного зв’язку між перезавантаженнями сторінки.
+  const [clicks, setClicks] = useState(() => {
+    const storedClicks = JSON.parse(window.localStorage.getItem("feedback"));
+    return storedClicks || { good: 0, neutral: 0, bad: 0 };
   });
 
+  // Ефект для оновлення локального сховища:
+  //Хук useEffect використовується для оновлення локального сховища кожного разу, коли змінюється стан кліків.
+  useEffect(() => {
+    window.localStorage.setItem("feedback", JSON.stringify(clicks));
+  }, [clicks]);
+  // Функції для оновлення відгуків
+  // Функція updateFeedback визначена для обробки оновлення кількості відгуків на основі типу наданого відгуку. Він приймає параметр feedbackType і збільшує відповідну властивість в об’єкті стану кліків на 1.
   const updateFeedback = (feedbackType) => {
-    console.log(feedbackType);
     setClicks({
       ...clicks,
       [feedbackType]: clicks[feedbackType] + 1,
     });
   };
-
+  // Функція feedbackReset скидає всі підрахунки відгуків до 0
   const feedbackReset = () => {
     setClicks({
       ...clicks,
@@ -92,9 +169,11 @@ export function App() {
       bad: (clicks.bad = 0),
     });
   };
+  // Змінна totalFeedback обчислює загальну кількість записів відгуків шляхом підсумовування значень
   const totalFeedback = clicks.good + clicks.neutral + clicks.bad;
   console.log(clicks);
   return (
+    // Rendering Components
     <>
       <Descriptions />
       <Options
--- src/components/Descriptions/FeedBack/FeedBack.jsx
+++ src/components/Descriptions/FeedBack/FeedBack.jsx
@@ -1,17 +1,17 @@
-//......................ПРАЦЮЮЧИЙ ВАРІАНТ1
-// export const FeedBack = ({ valueBad, valueNeutral, valueGood }) => {
-//   return (
-//     <>
-//       <ul>
-//         <li>Good:{valueGood}</li>
-//         <li>Neutral:{valueNeutral}</li>
-//         <li>Bad:{valueBad}</li>
-//         <li>Total:{valueGood + valueNeutral + valueBad}</li>
-//         <li>Positive:{}</li>
-//       </ul>
-//     </>
-//   );
-// };
+// //......................ПРАЦЮЮЧИЙ ВАРІАНТ1
+// // export const FeedBack = ({ valueBad, valueNeutral, valueGood }) => {
+// //   return (
+// //     <>
+// //       <ul>
+// //         <li>Good:{valueGood}</li>
+// //         <li>Neutral:{valueNeutral}</li>
+// //         <li>Bad:{valueBad}</li>
+// //         <li>Total:{valueGood + valueNeutral + valueBad}</li>
+// //         <li>Positive:{}</li>
+// //       </ul>
+// //     </>
+// //   );
+// // };
 
 export const FeedBack = ({ valueBad, valueNeutral, valueGood, valueTotal }) => {
   return (
